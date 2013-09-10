3ed1125dd3d509f951c126ca7d7a785dcac3123e
Fabio Rehm <fgrehm@gmail.com>
2013-09-10 19:59:13 -0300

    v0.1.0

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## 0.1.0 (September 10, 2013)
+
+  - Initial public release.
--- lib/ventriloquist/version.rb
+++ lib/ventriloquist/version.rb
@@ -1,5 +1,5 @@
 module VagrantPlugins
   module Ventriloquist
-    VERSION = "0.1.0.dev"
+    VERSION = "0.1.0"
   end
 end
