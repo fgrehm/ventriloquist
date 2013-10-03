92dec32fa31dfee85f971ef024a8579296509310
Fabio Rehm <fgrehm@gmail.com>
2013-10-03 19:42:20 -0300

    erlang: Make use of `download` capability to download initial .deb and allow caching with vagrant-cachier

--- lib/ventriloquist/cap/debian/erlang_install.rb
+++ lib/ventriloquist/cap/debian/erlang_install.rb
@@ -3,15 +3,33 @@ module VagrantPlugins
     module Cap
       module Debian
         module ErlangInstall
+          ERLANG_SOLUTIONS_PKG = "https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb"
+
           def self.erlang_install(machine)
             machine.communicate.tap do |comm|
               if ! comm.test('which erlang > /dev/null')
                 machine.env.ui.info('Installing Erlang')
-                comm.execute('wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb')
-                comm.sudo('dpkg -i erlang-solutions_1.0_all.deb')
+
+                path = download_path(comm)
+                unless comm.test("-f #{path}")
+                  machine.capability(:download, ERLANG_SOLUTIONS_PKG, path)
+                end
+                comm.sudo("dpkg -i #{path}")
+
                 comm.sudo('apt-get update')
                 comm.sudo('apt-get -y install erlang')
               end
+            end
+          end
+
+          private
+
+          def self.download_path(comm)
+            # If vagrant-cachier apt cache bucket is available, drop it there
+            if comm.test("test -d /tmp/vagrant-cache/apt")
+              "/tmp/vagrant-cache/apt/erlang-solutions_1.0_all.deb"
+            else
+              "/tmp/erlang-solutions_1.0_all.deb"
             end
           end
         end
