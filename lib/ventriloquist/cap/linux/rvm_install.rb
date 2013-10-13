f9c91855fdd56a87da92c8355531ecf1a45caf6f
Fabio Rehm <fgrehm@gmail.com>
2013-10-13 22:54:49 +0000

    Revert "Change to system wide rvm installation (closes GH-24)"
    This reverts commit b3799d57161f8fbd4f0a5615dea4bce0f0d8bee8.

--- lib/ventriloquist/cap/linux/rvm_install.rb
+++ lib/ventriloquist/cap/linux/rvm_install.rb
@@ -4,10 +4,10 @@ module VagrantPlugins
       module Linux
         module RvmInstall
           def self.rvm_install(machine)
-            if ! machine.communicate.test('test -d /usr/local/rvm')
+            if ! machine.communicate.test('test -d $HOME/.rvm')
               machine.env.ui.info('Installing RVM')
-              machine.communicate.sudo('\curl -L https://get.rvm.io | bash -s stable --autolibs=enabled')
-              machine.communicate.sudo('usermod -a -G rvm vagrant')
+              machine.communicate.execute('\curl -L https://get.rvm.io | bash -s stable --autolibs=enabled')
+              #machine.communicate.execute('rvm requirements')
             end
           end
         end
