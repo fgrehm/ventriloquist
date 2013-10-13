f90a732455319cb909affd603ef992133eed5e38
Fabio Rehm <fgrehm@gmail.com>
2013-10-13 23:02:44 +0000

    Manually manage rvm's dotfiles [GH-2]

--- lib/ventriloquist/cap/linux/rvm_install.rb
+++ lib/ventriloquist/cap/linux/rvm_install.rb
@@ -6,8 +6,8 @@ module VagrantPlugins
           def self.rvm_install(machine)
             if ! machine.communicate.test('test -d $HOME/.rvm')
               machine.env.ui.info('Installing RVM')
-              machine.communicate.execute('\curl -L https://get.rvm.io | bash -s stable --autolibs=enabled')
-              #machine.communicate.execute('rvm requirements')
+              machine.communicate.execute('\curl -L https://get.rvm.io | bash -s stable --autolibs=enabled --ignore-dotfiles')
+              machine.communicate.execute("echo 'source $HOME/.rvm/scripts/rvm' >> ~/.profile")
             end
           end
         end
