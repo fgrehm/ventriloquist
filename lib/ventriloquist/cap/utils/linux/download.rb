790311be8c383f027ea476ef2dfcddb9a334eb64
Fabio Rehm <fgrehm@gmail.com>
2014-04-05 01:10:14 -0300

    cap/download: Implement support for downloading files from multiple sources

--- lib/ventriloquist/cap/utils/linux/download.rb
+++ lib/ventriloquist/cap/utils/linux/download.rb
@@ -3,19 +3,19 @@ module VagrantPlugins
     module Cap
       module Linux
         module Download
-          def self.download(machine, src, destination)
+          def self.download(machine, srcs, destination)
             machine.communicate.tap do |comm|
               if comm.test('which wget')
-                download_with_wget(machine, src, destination)
+                download_with_wget(machine, srcs, destination)
 
               elsif comm.test('which curl')
-                download_with_curl(machine, src, destination)
+                download_with_curl(machine, srcs, destination)
 
               # If neither wget or curl could be found, try installing
               # curl
               elsif machine.capability?(:install_packages)
                 machine.guest.capability(:install_packages, 'curl')
-                download_with_curl(machine, src, destination)
+                download_with_curl(machine, srcs, destination)
 
               else
                 raise 'Unable to download file for guest VM!'
@@ -23,14 +23,28 @@ module VagrantPlugins
             end
           end
 
-          def self.download_with_wget(machine, src, destination)
-            machine.env.ui.info("Downloading #{src} to #{destination}")
-            machine.communicate.execute("wget #{src} -O #{destination}")
+          def self.download_with_wget(machine, srcs, destination)
+            Array(srcs).each_with_index do |src, idx|
+              machine.env.ui.info("Attempting to download '#{src}' to '#{destination}'")
+              begin
+                machine.communicate.execute("wget #{src} -O #{destination} || { rm -f #{destination} && exit 1; }")
+                break
+              rescue Vagrant::Errors::VagrantError => e
+                raise if idx + 1 == srcs.size
+              end
+            end
           end
 
-          def self.download_with_curl(machine, src, destination)
-            machine.env.ui.info("Downloading #{src} to #{destination}")
-            machine.communicate.execute("curl #{src} -o #{destination}")
+          def self.download_with_curl(machine, srcs, destination)
+            Array(srcs).each_with_index do |src, idx|
+              machine.env.ui.info("Attempting to download '#{src}' to '#{destination}'")
+              begin
+                machine.communicate.execute("curl #{src} -o #{destination} || { rm -f #{destination} && exit 1; }")
+                break
+              rescue Vagrant::Errors::VagrantError => e
+                raise if idx + 1 == srcs.size
+              end
+            end
           end
         end
       end
